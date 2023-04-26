18516f6e10d942435cee3ea25d40ef4e3b509f1a
midnightCODA <mbunjimo@gmail.com>
2023-04-26 20:40:15 +0300

    UI foundations are live

---
+++ Client/UI/src/components/Demo.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets";
+
+const Demo = () => {
+  return (
+    <section className='mt-16 w-full max-w-xl'>
+      {/* Search */}
+      <div className='flex flex-col w-full gap-2'>
+        <form
+          className='relative flex justify-center items-center'
+        >
+          <img
+            src={linkIcon}
+            alt='link-icon'
+            className='absolute left-0 my-2 ml-3 w-5'
+          />
+
+          <input
+            type='url'
+            placeholder='Paste the article link'
+            required
+            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+          />
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+          >
+            <p>↵</p>
+          </button>
+        </form>
+
+        {/* Browse History */}
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+        </div>
+      </div>
+
+      {/* Display Result */}
+      <div className='my-10 max-w-full flex justify-center items-center'>
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
